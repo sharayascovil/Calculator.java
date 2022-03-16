16d6cb72b5197d2d4b033a01925e9f14d8738748
ShaRaya <sharayascovil@u.northwestern.edu>
2022-03-15 21:31:25 -0600

    second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 // import java.util.Scanner;
 
 public class Calculator
@@ -36,5 +38,43 @@ public int square(int num1)
     }
 }
 
+class MagicCalculator extends Calculator
+
+{
+
+public static int factorial (int n)
+
+    {
+
+    return (n == 1 || n == 0) ? 1 : n * factorial (n - 1);
+
+    }
+
+
+    public static void main(String[], args) {
+
+    Calculator calculator = new Calculator();
+
+    Scanner sc = new Scanner (System.in);
+
+    System.out.print("Enter Number: ");
+    
+    int num = sc.nextInt();
+
+    int square = calculator.square(num);
+
+    System.out.println("The Square Root of " + num + " is: " + square);
+
+    System.out.print("Enter angle in degrees: ");
+
+        double degree = sc.nextDouble();
+
+        double radians = Math.toRadians(degree);
+
+    System.out.println("Sin is: " + Math.sin(radians));
+    System.out.println("Cosine is: " + Math.cos(radians));
+    System.out.println("Tangent is: " + Math.tan(radians));
+    System.out.println("Factorial of " + num + "is: " + factorial(num));
+    }
 
 
